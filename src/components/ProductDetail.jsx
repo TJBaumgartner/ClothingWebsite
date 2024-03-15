c2e4fb7f184362901767bef66305e010477f2bc4
Terrence <tnorth6@gmail.com>
2024-03-15 01:53:56 -0700

    product detail styling

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -5,12 +5,44 @@ import NavBanner from './NavBanner';
 
 function ProductDetail() {
     const location = useLocation();
-    const product = location.state.prdocut;
+    const product = location.state.product
     return (
     <>
         <NavBanner/>
-        <main className='productDetailContainer'>
-            
+        <main className='detailContainer'>
+            <div className='detailImageContainer'>
+                <img src={product.image}></img>
+            </div>
+            <div className='detailContent'>
+                <div className='detailTop'>
+                    {product.newArrival == true &&
+                        <h2>NEW!</h2>
+                    }
+                    <h1>{product.name}</h1>
+                    {product.onSale == true ?
+                            <div>
+                                <h2 className='clearanceCross'>${product.price}</h2>
+                                <h2 className='clearanceColor'>${Math.ceil(product.price*0.7)} Clearance</h2>
+                            </div>
+                            :
+                            <div>
+                                <h2>${product.price}</h2>
+                            </div>
+                    }
+                </div>
+                <div className='detailMiddle'>
+                    <input name='size' type='radio' id='XS'></input>
+                    <label for="XS">XS</label>
+                    <input name='size' type='radio' id='S'></input>
+                    <label for="S">S</label>
+                    <input name='size' type='radio' id='M'></input>
+                    <label for="M">M</label>
+                    <input name='size' type='radio' id='L'></input>
+                    <label for="L">L</label>
+                    <input name='size' type='radio' id='XL'></input>
+                    <label for="XL">XL</label>
+                </div>
+            </div>
         </main>
     </>
   )
