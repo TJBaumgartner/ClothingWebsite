95dfd5b5885ab4ec228da1fb2017ebd5176ec0b4
Terrence <tnorth6@gmail.com>
2024-03-23 12:49:53 -0700

    filter button on collections page

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -10,6 +10,8 @@ function Collections() {
 
   const [products, setProducts] = useState()
   const [sortDown, setSortDown] = useState(true)
+  const [filter, setFilter] = useState(true)
+
   const sex = useParams().sex
   const category = useParams().category
 
@@ -20,38 +22,79 @@ function Collections() {
   useEffect(() => {
     setProducts(Products)
   }, [])
-
   return (
   <>
     <NavBanner/>
+      <div className='filterContainer'>
+        {filter == true ? 
+          <select value={filter} onChange={() => setFilter(!filter)}>
+            <option value={"Price : High to Low"}>Price : High to Low</option>
+            <option value={"Price : Low to High"}>Price : Low to High</option>
+          </select>
+        :
+          <select value={filter} onChange={() => setFilter(!filter)}>
+            <option value={"Price : Low to High"}>Price : Low to High</option>
+            <option value={"Price : High to Low"}>Price : High to Low</option>
+          </select>
+        }
+      </div>
     <main className='collectionsContainer'>
       <Sidenav sex={sex}/>
-      <div className='collectionsContent'>
-      {category == null && products &&
-          products.sort((a, b) =>  b.price - a.price).map((product) => {
-            return product.gender == sex &&
+        {filter == true ?
+        <div className='collectionsContent'>
+          {category == null && products &&
+              products.sort((a, b) =>  b.price - a.price).map((product) => {
+                return product.gender == sex &&
+                <ProductCard product={product} key={product.id}></ProductCard>
+              })
+          }
+          {category == 'arrivals' && products &&
+            products.sort((a, b) =>  b.price - a.price).map((product) => {
+              return product.gender == sex && product.newArrival == true &&
+              <ProductCard product={product} key={product.id}></ProductCard>
+            })
+          }
+          {category == 'sale' && products &&
+            products.sort((a, b) =>  b.price - a.price).map((product) => {
+              return product.gender == sex && product.onSale == true &&
+              <ProductCard product={product} key={product.id}></ProductCard>
+            })
+          }
+          {products && category !== 'arrivals' && category !== 'sale' &&
+            products.sort((a, b) =>  b.price - a.price).map((product) => {
+              return product.gender == sex && product.category == category &&
+              <ProductCard product={product} key={product.id}></ProductCard>
+            })
+          }
+        </div>
+        :
+        <div className='collectionsContent'>
+        {category == null && products &&
+            products.sort((a, b) =>  a.price - b.price).map((product) => {
+              return product.gender == sex &&
+              <ProductCard product={product} key={product.id}></ProductCard>
+            })
+        }
+        {category == 'arrivals' && products &&
+          products.sort((a, b) =>  a.price - b.price).map((product) => {
+            return product.gender == sex && product.newArrival == true &&
             <ProductCard product={product} key={product.id}></ProductCard>
           })
-      }
-      {category == 'arrivals' && products &&
-        products.sort((a, b) =>  b.price - a.price).map((product) => {
-          return product.gender == sex && product.newArrival == true &&
-          <ProductCard product={product} key={product.id}></ProductCard>
-        })
-      }
-      {category == 'sale' && products &&
-        products.sort((a, b) =>  b.price - a.price).map((product) => {
-          return product.gender == sex && product.onSale == true &&
-          <ProductCard product={product} key={product.id}></ProductCard>
-        })
-      }
-      {products && category !== 'arrivals' && category !== 'sale' &&
-        products.sort((a, b) =>  b.price - a.price).map((product) => {
-          return product.gender == sex && product.category == category &&
-          <ProductCard product={product} key={product.id}></ProductCard>
-        })
-      }
+        }
+        {category == 'sale' && products &&
+          products.sort((a, b) =>  a.price - b.price).map((product) => {
+            return product.gender == sex && product.onSale == true &&
+            <ProductCard product={product} key={product.id}></ProductCard>
+          })
+        }
+        {products && category !== 'arrivals' && category !== 'sale' &&
+          products.sort((a, b) =>  a.price - b.price).map((product) => {
+            return product.gender == sex && product.category == category &&
+            <ProductCard product={product} key={product.id}></ProductCard>
+          })
+        }
       </div>
+        }
     </main>
   </>
   )
